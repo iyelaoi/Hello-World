5cbcadf1247cb9f8262f82151d3190025b25b5d5
iyelaoi <1360717190@qq.com>
2020-10-28 15:54:07 +0800

    commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Maven project");
+        System.out.println(StringUtils.endsWith("ssss", "ss"));
+    }
+}
